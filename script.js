0fbe60ad3c7eae8d3864c27823b5f520da7f1677
Ivandellam <ivandellamaggiore@gmail.com>
2022-08-30 18:05:54 -0300

    simuladorDeIva

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let i;
+let precio = 300;
+let ivaArg = 21
+let iva = precio *ivaArg/100
+let precioConIva = precio + iva;
 
-for(i=1;i<=20;i++)
-    if(i%2==0){
-        console.log(i);
-
-    }
+console.log(precioConIva)
