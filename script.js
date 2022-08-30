22c368c5b70338f2ad7e83c0d6b1fdeb86e54116
Ivandellam <ivandellamaggiore@gmail.com>
2022-08-30 19:42:06 -0300

    simulador

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let precio = 300;
-let ivaArg = 21
-let iva = precio *ivaArg/100
-let precioConIva = precio + iva;
+function calcularPrecio (precioPrenda, cantidadPrenda, porcentajeDescuento, costoEnvio) {
+    let descuento = (precioPrenda * porcentajeDescuento) / 100;
+    let precioConDescuento = precioPrenda - descuento;
+    return (precioConDescuento * cantidadPrenda) + costoEnvio;
+}
 
-console.log(precioConIva)
+const envio = 300;
+
+let articulo = parseFloat(prompt("Ingrese el precio de la prenda que compraras:"));
+let cantidad = parseInt(prompt("Cuantas prendas queres comprar?:"));
+let descuento = parseInt(prompt("Cupon de descuento:")) 
+
+
+let precioFinal = calcularPrecio(articulo, cantidad, descuento, envio);
+alert("El precio total de tu compra es $" + precioFinal);
+alert("¡Gracias por su compra!");
